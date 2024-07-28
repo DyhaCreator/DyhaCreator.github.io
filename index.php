1f91d212b9bfd4be677ad4273dbe336f6e5c8d06
Kapusta <91116965+DyhaCreator@users.noreply.github.com>
2024-07-28 12:50:08 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+        <p>Enter code</p>
+        <form action="index.php" method="POST">
+            <input name="code">
+        </form>
+        <?php
+            
+        ?>
+    </body>
+</html>
